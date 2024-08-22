5018da26e3214a1cf191e5a1a89fd66d45c3cf3b
Kul Ahad <kulahad007@gmail.com>
2024-08-22 16:22:57 +1200

    add url parser functions

--- lib/urlparser.ts
+++ lib/urlparser.ts
@@ -1,8 +1,12 @@
-//check if url exists in mongodb
-//if it does, give existing id, if not, create new one
+"use server";
 
 import { Collection } from "mongodb";
 import client from "./mongodb";
+import { z } from "zod";
+
+const schema = z
+  .string()
+  .url("Must be a valid url as such: https://example.org/");
 
 export const processURLfromUser = async (url: URL) => {
   var nurl;
@@ -12,11 +16,49 @@ export const processURLfromUser = async (url: URL) => {
   nurl = await urlsCollection.findOne({ url: url.toString() });
 
   if (!nurl) {
-    nurl = await urlsCollection.insertOne({
+    await urlsCollection.insertOne({
       url: url.toString(),
-      miniId: new Date().valueOf(),
+      miniId: String(new Date().valueOf()),
     });
+
+    nurl = await urlsCollection.findOne({ url: url.toString() });
   }
 
   return nurl;
 };
+
+export const getURLFromId = async (id: string) => {
+  if (!id) {
+    return null;
+  }
+
+  var nurl = await client.db("nurl").collection("urls").findOne({ miniId: id });
+
+  if (!nurl) {
+    return null;
+  }
+
+  return nurl.url;
+};
+
+export async function getDataFromForm(
+  prevstate: any,
+  formdata: FormData
+): Promise<{ message: string }> {
+  try {
+    const parsedURL = schema.parse(formdata.get("url"));
+    const id = await processURLfromUser(new URL(parsedURL));
+
+    return { message: "" };
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      for (const issue of error.issues) {
+        return { message: `Validation failed: ${issue.message}` };
+      }
+    } else {
+      return { message: `Unexpected error: ${error}` };
+    }
+  }
+
+  return { message: "" };
+}
